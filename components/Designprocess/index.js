35e4d01758651dc5c963b42c8dc1eeea7ccd1d16
ankit shiwakoti <shiwakotiankit@gmail.com>
2023-06-22 12:18:17 +0545

    design section

--- components/Designprocess/index.js
+++ components/Designprocess/index.js
@@ -36,13 +36,17 @@ const Designprocess = () => {
         <Row>
           {data?.map((items, index) => (
             <Col className="p-0 mt-4" xs={12} sm={6} md={6} lg={3} key={index}>
-              <div class="designhov  card rounded-0 p-0 ">
+              <div class="designhov  card border-opacity-25 rounded-0 p-0 ">
                 <div class="card-body testcolor pb-4">
                   <div
-                    className="hovsmall bg-primary mt-3 p-3 d-flex justify-content-center align-items-center"
-                    style={{ width: "100px", color: "white" }}
+                    className="hovsmall  mt-3 p-1 d-flex justify-content-center align-items-center"
+                    style={{
+                      width: "100px",
+
+                      backgroundColor: "#FFFFFF",
+                    }}
                   >
-                    <h1 className="">{items.num}</h1>
+                    <h1 className=" fw-medium">{items.num}</h1>
                   </div>
                   <h3 class=" mt-4 p-2 ps-0 p fw-medium ">{items.title}</h3>
                   <p class=" mt-4 p fw-normal " style={{ color: "#525252" }}>
