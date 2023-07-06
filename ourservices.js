c35ceef75f87e7b2b147fa644b1eebe2bb357a07
anup05 <anup05@copenned.com>
2023-07-06 14:41:58 +0545

    ok

--- ourservices.js
+++ ourservices.js
@@ -1,45 +1,39 @@
-import serviceImage1 from "./public/assets/ourservices/Service1.png";
-import serviceImage2 from "./public/assets/ourservices/Service2.png";
-import serviceImage3 from "./public/assets/ourservices/Service3.png";
-import serviceImage4 from "./public/assets/ourservices/Service4.png";
-import serviceImage5 from "./public/assets/ourservices/Service5.png";
-import serviceImage6 from "./public/assets/ourservices/Service6.png";
 
 export const data = [
   {
     id: 1,
     title: "BIM MODELING ",
     body: "With the help of our talented pool of interior designers and architects, we provide unparallel interior design solutions that leave our clients content  and satisfied. Our services cover a wide array of requirements, ranging  from  designs to furniture placement color selection With the help of our talented pool of interior designers and architects, we  provide unparallel interior design solutions that leave our clients content and satisfied. Our services cover a wide array of requirements, ranging from  designs to furniture placement color selection ",
-    image: serviceImage1,
+    image: '/assets/ourservices/Service1.png',
   },
   {
     id: 2,
     title: "AUTOCAD DRAFTING",
     body: "With the help of our talented pool of interior designers and architects, we provide unparallel interior design solutions that leave our clients content and satisfied. Our services cover a wide array of requirements, ranging from  designs to furniture placement color selection.It leave our clients content and satisfied. Our services cover a wide array of requirements, ranging from  designs to furniture placement color selection ",
-    image: serviceImage2,
+    image: '/assets/ourservices/Service2.png',
   },
   {
     id: 3,
     title: "Interior Design ",
     body: "With the help of our talented pool of interior designers and architects, we provide unparallel interior design solutions that leave our clients content  and satisfied. Our services cover a wide array of requirements, ranging  from  designs to furniture placement color selection With the help of our talented pool of interior designers and architects, we  provide unparallel interior design solutions that leave our clients content and satisfied. Our services cover a wide array of requirements, ranging from  designs to furniture placement color selection ",
-    image: serviceImage3,
+    image: '/assets/ourservices/Service3.png',
   },
   {
     id: 4,
     title: "3D MODELING AND RENDERING/ FACADE DESIGN ",
     body: "With the help of our talented pool of interior designers and architects, we provide unparallel interior design solutions that leave our clients content  and satisfied. Our services cover a wide array of requirements, ranging  from  designs to furniture placement color selection With the help of our talented pool of interior designers and architects, we  provide unparallel interior design solutions that leave our clients content and satisfied. Our services cover a wide array of requirements, ranging from  designs to furniture placement color selection ",
-    image: serviceImage4,
+    image: '/assets/ourservices/Service4.png',
   },
   {
     id: 5,
     title: "LANDSCAPE CONCEPT: TRANSFORMING OUTDOOR SPACE ",
     body: "With the help of our talented pool of interior designers and architects, we provide unparallel interior design solutions that leave our clients content  and satisfied. Our services cover a wide array of requirements, ranging  from  designs to furniture placement color selection With the help of our talented pool of interior designers and architects, we  provide unparallel interior design solutions that leave our clients content and satisfied. Our services cover a wide array of requirements, ranging from  designs to furniture placement color selection ",
-    image: serviceImage5,
+    image: '/assets/ourservices/Service5.png',
   },
   {
     id: 6,
     title: "ESTIMATION: STRATEGIC RESOURCE PLANNING ",
     body: "With the help of our talented pool of interior designers and architects, we provide unparallel interior design solutions that leave our clients content  and satisfied. Our services cover a wide array of requirements, ranging  from  designs to furniture placement color selection With the help of our talented pool of interior designers and architects, we  provide unparallel interior design solutions that leave our clients content and satisfied. Our services cover a wide array of requirements, ranging from  designs to furniture placement color selection ",
-    image:serviceImage6,
+    image:'/assets/ourservices/Service6.png',
   },
 ];
